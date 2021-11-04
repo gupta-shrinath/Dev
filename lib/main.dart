b22ded5ba60b315471226566fdf381317e60fb18
Gupta Shrinath <guptashrinath9@gmail.com>
2021-11-04 19:13:38 +0530

    Refactor main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
-import 'package:dev/tabs/camera_tab.dart';
+import 'package:dev/tabs/battery_tab.dart';
+import 'package:dev/tabs/network_tab.dart';
+import 'package:dev/tabs/system_tab.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'tabs/battery_tab.dart';
 import 'tabs/hardware_tab.dart';
-import 'tabs/system_tab.dart';
 
-void main() => runApp(DevCheck());
+void main() => runApp(
+      DevCheck(),
+    );
 
 class DevCheck extends StatefulWidget {
   @override
@@ -15,7 +17,7 @@ class DevCheck extends StatefulWidget {
 
 class _DevCheckState extends State<DevCheck>
     with SingleTickerProviderStateMixin {
-  TabController mTabController;
+  late TabController mTabController;
 
   @override
   void initState() {
@@ -59,14 +61,18 @@ class _DevCheckState extends State<DevCheck>
             controller: mTabController,
           ),
         ),
-        backgroundColor: Colors.grey.shade800,
+        //backgroundColor: Colors.grey.shade800,
         body: TabBarView(
           controller: mTabController,
           children: <Widget>[
             Hardware(),
             System(),
-            Battery(),
-            Camera(),
+            DeviceBattery(),
+            Network(),
+
+            // System(),
+            // Battery(),
+            // Camera(),
           ],
         ),
       ),
