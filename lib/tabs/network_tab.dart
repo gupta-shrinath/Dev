3fdc5371708cadf5e4b070bf7a2473f1da8de3ef
Gupta Shrinath <guptashrinath9@gmail.com>
2021-11-04 19:12:45 +0530

    Update network tab

---
+++ lib/tabs/network_tab.dart
@@ -0,0 +1,48 @@
+import 'package:dev/utils/android_specification.dart';
+import 'package:dev/widget/specification_card.dart';
+import 'package:flutter/material.dart';
+
+class Network extends StatelessWidget {
+  const Network({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Wifi(),
+        ],
+      ),
+    );
+  }
+}
+
+class Wifi extends StatelessWidget {
+  const Wifi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: AndroidSpecification().getWifiNetworkSpecifications(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.hasData) {
+            return SpecificationCard(
+              title: 'WIFI',
+              content: Column(
+                children: buildWidgetsFromDeviceSpecs(
+                    deviceSpecs: snapshot.data as Map),
+              ),
+            );
+          } else {
+            return Center(
+              child: Text('Failed to get the specs. Restart the app'),
+            );
+          }
+        });
+  }
+}
