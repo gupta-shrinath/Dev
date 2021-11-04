e3d2bea2da2ccf3138a4ff64bd95aaf59e4f275d
Gupta Shrinath <guptashrinath9@gmail.com>
2021-11-04 19:11:12 +0530

    Update hardware tab

--- lib/tabs/hardware_tab.dart
+++ lib/tabs/hardware_tab.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
-
+import 'package:dev/utils/android_specification.dart';
+import 'package:dev/widget/specification_card.dart';
 import 'package:flutter/material.dart';
-
-import '../device_info.dart';
 
 class Hardware extends StatelessWidget {
   @override
@@ -17,345 +15,34 @@ class Hardware extends StatelessWidget {
   }
 }
 
-class Processor extends StatefulWidget {
-  @override
-  _ProcessorState createState() => _ProcessorState();
-}
-
-class _ProcessorState extends State<Processor> {
-  // Style Vars //
-  final titleFontSize = 20.0;
-  final contentFontSize = 16.0;
-  static const padding = 20.0;
-
-  List processorInfoList = [
-    'Loading',
-    'Loading',
-    'Loading',
-    'Loading',
-    'Loading',
-    'Loading',
-    'Loading',
-    'Loading',
-    'Loading'
-  ];
-
-  var mDeviceInfo = DeviceInfo();
-
-  @override
-  void initState() {
-    super.initState();
-    mDeviceInfo.initPlatformState().then((value) {
-      setState(() {
-        processorInfoList[0] = mDeviceInfo.getDeviceData('hardware');
-        processorInfoList[1] = mDeviceInfo.getDeviceData('board');
-        processorInfoList[4] = Platform.numberOfProcessors.toString();
-        processorInfoList[6] = mDeviceInfo.getDeviceData('supported32BitAbis');
-        processorInfoList[7] = mDeviceInfo.getDeviceData('supported64BitAbis');
-        processorInfoList[8] = mDeviceInfo.getDeviceData('supportedAbis');
-      });
-    });
-  } // Device Info //
-
+class Processor extends StatelessWidget {
+  const Processor({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: <Widget>[
-          Row(children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: padding),
-              child: Text(
-                'PROCESSOR',
-                style: TextStyle(
-                  color: Colors.green,
-                  fontSize: titleFontSize,
-                ),
-              ),
-            )
-          ]),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 50.0),
-                  child: Image.asset(
-                    'images/processor-icon.png',
-                  ),
-                ),
-              ),
-              Expanded(
-                  child: Text(
-                processorInfoList[0],
-                style: TextStyle(
-                  color: Colors.green,
-                  fontSize: titleFontSize,
-                ),
-              )),
-            ],
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: padding),
-                  child: Text(
-                    'Hardware',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: titleFontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Text(
-                    processorInfoList[1],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: contentFontSize,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: padding),
-                  child: Text(
-                    'CPU',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: titleFontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Text(
-                    processorInfoList[2],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: contentFontSize,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: padding),
-                  child: Text(
-                    'Process',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: titleFontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Text(
-                    processorInfoList[3],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: contentFontSize,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: padding),
-                  child: Text(
-                    'Cores',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: titleFontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Text(
-                    processorInfoList[4],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: contentFontSize,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: padding),
-                  child: Text(
-                    'Frequencies',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: titleFontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Text(
-                    processorInfoList[5],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: contentFontSize,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: padding),
-                  child: Text(
-                    'Support 32 bit ABIs',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: titleFontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Text(
-                    processorInfoList[6],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: contentFontSize,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: padding),
-                  child: Text(
-                    'Support 64 bit ABIs',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: titleFontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Text(
-                    processorInfoList[7],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: contentFontSize,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: padding),
-                  child: Text(
-                    'Supported ABIs',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: titleFontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Text(
-                    processorInfoList[8],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: contentFontSize,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+    return FutureBuilder(
+      future: AndroidSpecification().getProcessorSpecifications(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.hasData) {
+          return SpecificationCard(
+            title: 'PROCESSOR',
+            content: Column(
+              children: buildWidgetsFromDeviceSpecs(
+                  deviceSpecs: snapshot.data as Map),
+            ),
+            imageText: 'PP',
+            imageUrl: 'images/processor-icon.png',
+          );
+        } else {
+          return Center(
+            child: Text('Failed to get the specs. Restart the app'),
+          );
+        }
+      },
     );
   }
 }
