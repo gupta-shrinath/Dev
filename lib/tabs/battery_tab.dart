10eaf9bd7c89eb6d3d6cde559ae54fa06f117e6f
Gupta Shrinath <guptashrinath9@gmail.com>
2021-11-08 21:31:15 +0530

    Refactor to use battery_info package

--- lib/tabs/battery_tab.dart
+++ lib/tabs/battery_tab.dart
@@ -1,4 +1,8 @@
-import 'package:battery_plus/battery_plus.dart';
+import 'package:battery_info/battery_info_plugin.dart';
+import 'package:battery_info/model/android_battery_info.dart';
+import 'package:dev/utils/android_specification.dart';
+import 'package:dev/utils/system_information_builder.dart';
+import 'package:dev/widgets/specification_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -41,15 +45,16 @@ class _DeviceBatteryState extends State<DeviceBattery> {
           children: <Widget>[
             Padding(
               padding: EdgeInsets.symmetric(horizontal: padding),
-              child: FutureBuilder(
-                future: Battery().batteryLevel,
+              child: FutureBuilder<AndroidBatteryInfo?>(
+                future: BatteryInfoPlugin().androidBatteryInfo,
                 builder: (context, AsyncSnapshot snapshot) {
                   if (snapshot.connectionState != ConnectionState.done) {
                     return CircularProgressIndicator.adaptive();
                   } else if (snapshot.connectionState == ConnectionState.done &&
                       snapshot.hasData) {
+                    AndroidBatteryInfo? _androidInfo = snapshot.data;
                     return Text(
-                      snapshot.data.toString() + '%',
+                      _androidInfo!.batteryLevel.toString() + '%',
                       style: TextStyle(
                         color: Colors.green,
                         fontWeight: FontWeight.bold,
@@ -72,292 +77,66 @@ class _DeviceBatteryState extends State<DeviceBattery> {
             SizedBox(
               width: 20.0,
             ),
-            Column(
-              children: <Widget>[
-                StreamBuilder<BatteryState>(
-                    stream: Battery().onBatteryStateChanged,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return Text(
-                          snapshot.data
+            StreamBuilder(
+                stream: BatteryInfoPlugin().androidBatteryInfoStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    AndroidBatteryInfo? _androidBatteryInfo =
+                        snapshot.data as AndroidBatteryInfo?;
+                    return Column(
+                      children: [
+                        Text(
+                          _androidBatteryInfo!.chargingStatus
                               .toString()
-                              .replaceAll('BatteryState.', '')
-                              .toUpperCase(),
+                              .replaceFirst("ChargingStatus.", ""),
                           style: TextStyle(
                             color: Colors.green,
                             fontWeight: FontWeight.bold,
                             fontSize: 18.0,
                           ),
-                        );
-                      } else {
-                        return CircularProgressIndicator();
-                      }
-                    }),
-                Text(
-                  'Current ' + ' b',
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                  ),
-                ),
-              ],
-            ),
-            // FutureBuilder(
-            //     future:
-            //         AndroidSpecification().getOperatingSystemSpecifications(),
-            //     builder: (context, snapshot) {
-            //       if (snapshot.connectionState != ConnectionState.done) {
-            //         return Center(
-            //           child: CircularProgressIndicator(),
-            //         );
-            //       } else if (snapshot.connectionState == ConnectionState.done &&
-            //           snapshot.hasData) {
-            //         return SpecificationCard(
-            //           title: 'STATUS',
-            //           content: Column(
-            //             children: buildWidgetsFromDeviceSpecs(
-            //                 deviceSpecs: snapshot.data as Map),
-            //           ),
-            //         );
-            //       } else {
-            //         return Center(
-            //           child: Text('Failed to get the specs. Restart the app'),
-            //         );
-            //       }
-            //     }),
+                        ),
+                        Text(
+                          'Current ' +
+                              (_androidBatteryInfo.currentNow! / 1000)
+                                  .toStringAsFixed(0) +
+                              ' mA',
+                          style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ],
+                    );
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                }),
           ],
+        ),
+        FutureBuilder(
+          future: AndroidSpecification().getBatterySpecifications(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState != ConnectionState.done) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData) {
+              return SpecificationCard(
+                title: 'INFORMATION',
+                content: Column(
+                  children: systemInformationBuilder(snapshot.data as Map),
+                ),
+              );
+            } else {
+              return Center(
+                child: Text('Failed to get the specs. Restart the app'),
+              );
+            }
+          },
         ),
       ],
     );
   }
 }
-// return Column(
-//   children: <Widget>[
-//     Row(
-//       children: <Widget>[
-//         Padding(
-//           padding: const EdgeInsets.symmetric(
-//               horizontal: padding, vertical: padding),
-//           child: Text(
-//             'STATUS',
-//             style: TextStyle(
-//               color: Colors.green,
-//               fontSize: titleFontSize,
-//             ),
-//           ),
-//         )
-//       ],
-//     ),
-//     Row(
-//       children: <Widget>[
-//         Padding(
-//           padding: EdgeInsets.symmetric(horizontal: padding),
-//           child: Text(
-//             '',
-//             style: TextStyle(
-//               color: Colors.green,
-//               fontSize: 50.0,
-//               fontWeight: FontWeight.bold,
-//             ),
-//           ),
-//         ),
-//         SizedBox(
-//           width: 20.0,
-//         ),
-//         Column(
-//           children: <Widget>[
-//             Text(
-//               '',
-//               style: TextStyle(
-//                 color: Colors.green,
-//                 fontWeight: FontWeight.bold,
-//                 fontSize: 18.0,
-//               ),
-//             ),
-//             Text(
-//               'Current ',
-//               style: TextStyle(
-//                 color: Colors.green,
-//                 fontWeight: FontWeight.bold,
-//                 fontSize: 18.0,
-//               ),
-//             ),
-//           ],
-//         ),
-//       ],
-//     ),
-//     Row(children: <Widget>[
-//       Padding(
-//         padding: const EdgeInsets.only(left: padding, top: padding),
-//         child: Text(
-//           'INFORMATION',
-//           style: TextStyle(
-//             color: Colors.green,
-//             fontSize: titleFontSize,
-//           ),
-//         ),
-//       )
-//     ]),
-//     SizedBox(
-//       height: 20.0,
-//     ),
-//     Row(
-//       children: <Widget>[
-//         Expanded(
-//           child: Padding(
-//             padding: EdgeInsets.symmetric(horizontal: padding),
-//             child: Text(
-//               'Temperature',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: titleFontSize,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//           ),
-//         ),
-//         Expanded(
-//           child: Padding(
-//             padding: EdgeInsets.all(5.0),
-//             child: Text(
-//               '',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: contentFontSize,
-//               ),
-//             ),
-//           ),
-//         ),
-//       ],
-//     ),
-//     SizedBox(
-//       height: 20.0,
-//     ),
-//     Row(
-//       children: <Widget>[
-//         Expanded(
-//           child: Padding(
-//             padding: EdgeInsets.symmetric(horizontal: padding),
-//             child: Text(
-//               'Technology',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: titleFontSize,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//           ),
-//         ),
-//         Expanded(
-//           child: Padding(
-//             padding: EdgeInsets.all(5.0),
-//             child: Text(
-//               '',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: contentFontSize,
-//               ),
-//             ),
-//           ),
-//         ),
-//       ],
-//     ),
-//     SizedBox(
-//       height: 20.0,
-//     ),
-//     Row(
-//       children: <Widget>[
-//         Expanded(
-//           child: Padding(
-//             padding: EdgeInsets.symmetric(horizontal: padding),
-//             child: Text(
-//               'Health',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: titleFontSize,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//           ),
-//         ),
-//         Expanded(
-//           child: Padding(
-//             padding: EdgeInsets.all(5.0),
-//             child: Text(
-//               '',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: contentFontSize,
-//               ),
-//             ),
-//           ),
-//         ),
-//       ],
-//     ),
-//     SizedBox(
-//       height: 20.0,
-//     ),
-//     Row(
-//       children: <Widget>[
-//         Expanded(
-//           child: Padding(
-//             padding: EdgeInsets.symmetric(horizontal: padding),
-//             child: Text(
-//               'Voltage',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: titleFontSize,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//           ),
-//         ),
-//         Expanded(
-//           child: Padding(
-//             padding: EdgeInsets.all(5.0),
-//             child: Text(
-//               '',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: contentFontSize,
-//               ),
-//             ),
-//           ),
-//         ),
-//       ],
-//     ),
-//     SizedBox(
-//       height: 20.0,
-//     ),
-//     Row(
-//       children: <Widget>[
-//         Expanded(
-//           child: Padding(
-//             padding: EdgeInsets.symmetric(horizontal: padding),
-//             child: Text(
-//               'Capacity (reported by system)',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: titleFontSize,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//           ),
-//         ),
-//         Expanded(
-//           child: Padding(
-//             padding: EdgeInsets.all(5.0),
-//             child: Text(
-//               '',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: contentFontSize,
-//               ),
-//             ),
-//           ),
-//         ),
-//       ],
-//     ),
-//   ],
-// );
